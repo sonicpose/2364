847ac0bd6e943564b6581c55fc51fe871432eef1
Dan Roebuck <danroebuck@mac.com>
2016-08-11 15:55:41 +0100

    Finnally Added Playlist And fixed bugs

--- startup.py
+++ startup.py
@@ -6,8 +6,12 @@ import re
 import sys
 
 globalInteger = 0
+otherGlobalInteger = 0
+downloadDir = "~/Downloads"
 
 def main():
+	clear()
+
 	create()
 	data = core.read('info')
 	core.log('log', data)
@@ -18,12 +22,12 @@ def main():
 	core.log('log', playlistUrls)
 	playlistUrls = core.split(playlistUrls)
 
-	#playlist(playlistUrls)
 	search(data)
 
-	videos = core.read('playlistUrls')
+	videos = core.read('videos')
 	core.log('log', videos)
 	videos = core.split(videos)
+
 
 	download(data, videos)
 
@@ -34,13 +38,13 @@ def create():
 	location = 'Placeholder'
 
 	while len(location) > 0:
-		idct = input("Directory>")
-		if len(idct) > 0:
-			dct = idct
+		#idct = input("Directory>")
+		#if len(idct) > 0:
+		#	dct = idct
 		location = input("Keyword> ")
 		if len(location) > 0:
 			globalInteger = globalInteger + 1
-			data ="{}\n{}".format(dct, location)
+			data ="{}\n{}".format(downloadDir, location)
 			core.log('info', data)
 
 	data = core.read('info')
@@ -48,16 +52,16 @@ def create():
 	return
 
 def createPlaylist():
-	global globalInteger
+	global otherGlobalInteger
 	playlistUrl = 'Placeholder'
 	while len(playlistUrl) > 0:
-		idct = input("Directory>")
-		if len(idct) > 0:
-			dct = idct
+		#idct = input("Directory>")
+		#if len(idct) > 0:
+		#	dct = idct
 		playlistUrl = input("Playlist Url> ")
 		if len(playlistUrl) > 0:
-			globalInteger = globalInteger + 1
-			playlistUrls ="{}\n{}".format(dct, playlistUrl)
+			otherGlobalInteger = otherGlobalInteger + 1
+			playlistUrls ="{}\n{}".format(downloadDir, playlistUrl)
 			core.log('playlistUrls', playlistUrls)
 			playlist(playlistUrl)
 
@@ -76,6 +80,7 @@ def search(data):
 		query_string = urllib.parse.urlencode({"search_query" : location})
 		html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
 		search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+		print("Adding video1: " + search_results[searchInteger])
 		core.log('videos',("http://www.youtube.com/watch?v=" + search_results[0]))
 		ii = ii + 2
 		il = il - 1
@@ -85,35 +90,47 @@ def search(data):
 	return
 
 def playlist(playlistUrl):
-	print("****************" + playlistUrl)
-	#query_string = urllib.parse.urlencode({playlistUrls})
 	html_content = urllib.request.urlopen(playlistUrl)
-	#print("***************" + html_content.read().decode())
 	search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-	#print("*****************" + ', '.join(search_results))
+
+	#print(', '.join(search_results))
 
 	localInteger = len(search_results)
+	#print("sr ", localInteger)
+
 	searchInteger = 0
 	while localInteger > 0:
-		core.log('videos', search_results[searchInteger])
+		print("Adding video2: " + search_results[searchInteger])
+		core.log('videos', "http://www.youtube.com/watch?v=" + search_results[searchInteger])
 		localInteger = localInteger - 1
 		searchInteger = searchInteger + 1
 
-		return
+	return
 
 def download(data, videos):
-	global globalInteger
-	string = 'cd {}' + '\n' + 'youtube-dl {}'
-	il = globalInteger
-	di = 0
-	vi = 1
+	#global globalInteger
+	cmdString = 'cd {}' + '\n' + '/usr/local/bin/youtube-dl {}'
+	print(', '.join(videos))
+	#il = len(videos)
+	#di = 0
+	vi = 0
+	videos = list(set(videos))
 
-	while il > 0:
-		dct = data[di]
+	#print("start il = " , il)
+
+	while vi < len(videos):
+		print("vi: ", vi)
+		#print("il = " , il)
+		#dct = data[di]
 		download = videos[vi]
-		os.system(string.format(dct, download))
-		il = il - 1
-		di = di + 2
+		print("Download: " + download)
+		cmd = cmdString.format(downloadDir, download)
+		print("******************************" + cmd)
+
+
+		os.system(cmd)
+		#il = il - 1
+		#di = di + 2
 		vi = vi + 1
 
 def log(filefrom, fileto):
@@ -123,5 +140,6 @@ def log(filefrom, fileto):
 def clear():
 	core.write('info', '')
 	core.write('videos', '')
+	core.write('playlistUrls', '')
 
 main()
